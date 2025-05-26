6eab1419efc7f29ce9c9040504f8b67469fa7ea7
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-05-26 19:04:54 -0230

    Added attributes and constructor to VideoGame.java

--- src/main/java/com/keyin/VideoGame.java
+++ src/main/java/com/keyin/VideoGame.java
@@ -1,4 +1,27 @@
 package com.keyin;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class VideoGame {
+
+    // Attributes
+
+    public String gameTitle;
+    public String gameDeveloper;
+    public LocalDate gameReleaseDate;
+    public double gamePrice;
+    public ArrayList<String>[] gamePlatforms;
+    public ArrayList<String>[] gameGenres;
+
+    // Constructor
+
+    public VideoGame(String gameTitle, String gameDeveloper, LocalDate gameReleaseDate, double gamePrice, ArrayList<String>[] gamePlatforms, ArrayList<String>[] gameGenres){
+        this.gameTitle = gameTitle;
+        this.gameDeveloper = gameDeveloper;
+        this.gameReleaseDate = gameReleaseDate;
+        this.gamePrice = gamePrice;
+        this.gamePlatforms = gamePlatforms;
+        this.gameGenres = gameGenres;
+    }
 }
